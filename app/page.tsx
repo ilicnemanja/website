a2307108e716a55201e7d7e156a5d855f0460132
Nemanja <ilicnemanja.it@gmail.com>
2024-11-09 15:40:57 +0100

    added technology section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,335 @@
 import Intro from "@/components/section/intro";
+import Technologies from "@/components/section/technologies";
 
 const Home = () => {
   return (
     <>
       <Intro />
+      <Technologies />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+        exercitationem qui nihil quis deleniti ad amet ex! Blanditiis,
+        architecto. Saepe libero blanditiis odit doloribus id quasi fugiat.
+        Aperiam, ullam excepturi!
+      </p>
     </>
   );
 };
---
+++ components/section/technologies/index.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import React from "react";
+
+const Technologies = () => {
+  return (
+    <div className="mb-24 mt-36 bg-[#f5f5f5] py-8 dark:bg-[#7272728e]">
+      <h1 className="mb-2 text-center font-sansation text-xl text-primary-100 dark:text-secondary-100">
+        Using the latest technologies
+      </h1>
+      <div className="mt-7 grid h-auto grid-cols-1 gap-8 px-14 md:h-40 md:grid-cols-5 md:gap-2">
+        <div className="flex items-center justify-center">
+          <Image
+            src="/images/typescript.svg"
+            alt="Next"
+            width={80}
+            height={40}
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <Image src="/images/next.svg" alt="Next" width={180} height={40} />
+        </div>
+        <div className="flex items-center justify-center">
+          <Image src="/images/nest.svg" alt="Next" width={180} height={40} />
+        </div>
+        <div className="flex items-center justify-center">
+          <Image src="/images/react.svg" alt="Next" width={100} height={40} />
+        </div>
+        <div className="flex items-center justify-center">
+          <Image src="/images/mongodb.svg" alt="Next" width={180} height={40} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Technologies;
