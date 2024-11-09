eb21d84b44961c6c2536a2e7f2b2809dbeb655c3
Nemanja <ilicnemanja.it@gmail.com>
2024-11-09 18:24:53 +0100

    finished technology section

--- components/section/technologies/index.tsx
+++ components/section/technologies/index.tsx
@@ -3,11 +3,19 @@ import React from "react";
 
 const Technologies = () => {
   return (
-    <div className="mb-24 mt-36 bg-[#f5f5f5] py-8 dark:bg-[#8a898975]">
+    <div className="mb-24 mt-36 bg-[#f5f5f5] py-8 dark:bg-[#232323]">
       <h1 className="mb-2 text-center font-sansation text-xl text-primary-100 dark:text-secondary-100">
         Using the latest technologies
       </h1>
-      <div className="mt-7 grid h-auto grid-cols-1 gap-8 px-14 md:h-40 md:grid-cols-5 md:gap-2">
+      <div className="md:h-38 my-7 grid h-auto grid-cols-2 gap-4 px-14 md:grid-cols-6 md:gap-2">
+        <div className="b flex items-center justify-center">
+          <Image
+            src="/images/javascript.svg"
+            alt="Next"
+            width={80}
+            height={40}
+          />
+        </div>
         <div className="flex items-center justify-center">
           <Image
             src="/images/typescript.svg"
@@ -15,6 +23,9 @@ const Technologies = () => {
             width={80}
             height={40}
           />
+        </div>
+        <div className="flex items-center justify-center">
+          <Image src="/images/python.svg" alt="Next" width={80} height={40} />
         </div>
         <div className="flex items-center justify-center">
           <Image src="/images/next.svg" alt="Next" width={180} height={40} />
@@ -25,10 +36,11 @@ const Technologies = () => {
         <div className="flex items-center justify-center">
           <Image src="/images/react.svg" alt="Next" width={100} height={40} />
         </div>
-        <div className="flex items-center justify-center">
-          <Image src="/images/mongodb.svg" alt="Next" width={180} height={40} />
-        </div>
       </div>
+
+      <h1 className="text-center font-sansation text-base text-primary-100 dark:text-secondary-100">
+        and many more...
+      </h1>
     </div>
   );
 };
